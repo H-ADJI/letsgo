df41ec6500d62104da4795904481780a007732f5
khalil hadji <h-adji_tech@proton.me>
2025-06-02 10:13:45 +0200

    groupe snippets routes

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -10,10 +10,14 @@ func (a app) routes() http.Handler {
 	mux := http.NewServeMux()
 
 	dynamic := alice.New(a.sessionManager.LoadAndSave)
+
 	mux.Handle("GET /{$}", dynamic.ThenFunc(a.home))
-	mux.Handle("GET /snippet/view/{id}", dynamic.ThenFunc(a.snippetView))
-	mux.Handle("GET /snippet/create", dynamic.ThenFunc(a.snippetCreate))
-	mux.Handle("POST /snippet/create", dynamic.ThenFunc(a.snippetCreatePost))
+
+	snippetMux := http.NewServeMux()
+	snippetMux.HandleFunc("GET /view/{id}", a.snippetView)
+	snippetMux.HandleFunc("POST /create", a.snippetCreatePost)
+	snippetMux.HandleFunc("GET /create", a.snippetCreate)
+	mux.Handle("/snippet/", http.StripPrefix("/snippet", dynamic.Then(snippetMux)))
 
 	fileserver := http.FileServer(http.Dir("./ui/static"))
 	mux.Handle("GET /static/", http.StripPrefix("/static", disableDirList(fileserver)))
