7844773c96c72cfcf857cefe0032eef2544a366a
khalil hadji <h-adji_tech@proton.me>
2025-06-05 09:33:55 +0200

    refac e2e test

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,33 +1,17 @@
 package main
 
 import (
-	"bytes"
-	"io"
-	"log/slog"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 
 	"github.com/H-ADJI/letsgo/internal/assert"
 )
 
 func TestPing(t *testing.T) {
-	a := &app{
-		logger: slog.New(slog.NewTextHandler(io.Discard, nil)),
-	}
-	ts := httptest.NewTLSServer(a.routes())
+	a := newTestApp(t)
+	ts := newTestServer(t, a.routes())
 	defer ts.Close()
-	res, err := ts.Client().Get(ts.URL + "/ping")
-	if err != nil {
-		t.Fatal(err)
-	}
-	assert.Equal(t, res.StatusCode, http.StatusOK)
-	defer res.Body.Close()
-	body, err := io.ReadAll(res.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-	body = bytes.TrimSpace([]byte(body))
-	assert.Equal(t, string(body), "OK")
-
+	statusCode, _, body := ts.get(t, "/ping")
+	assert.Equal(t, statusCode, http.StatusOK)
+	assert.Equal(t, body, "OK")
 }
---
+++ cmd/web/testutils_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"log/slog"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func newTestApp(t *testing.T) *app {
+	a := &app{
+		logger: slog.New(slog.NewTextHandler(io.Discard, nil)),
+	}
+	return a
+}
+
+type testServer struct {
+	*httptest.Server
+}
+
+func newTestServer(t *testing.T, h http.Handler) *testServer {
+	ts := httptest.NewTLSServer(h)
+	return &testServer{ts}
+}
+func (ts testServer) get(t *testing.T, urlPath string) (int, http.Header, string) {
+
+	res, err := ts.Client().Get(ts.URL + urlPath)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	body = bytes.TrimSpace([]byte(body))
+
+	return res.StatusCode, res.Header, string(body)
+}
